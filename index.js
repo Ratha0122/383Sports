27afcd6c93291ceee5964427abd26550ed0afeb3
Ratha0122 <thoeurnratha1@gmail.com>
2024-06-15 09:36:30 +0700

    update

--- index.js
+++ index.js
@@ -21,3 +21,29 @@ moblie_icon.addEventListener('click', openCloseMenu)
 function url(){
     location.href = "https://www.hay88.one/?inviteCode=5289036"
 }
+
+
+// Select the button element by its ID
+const btn = document.querySelector('#button');
+
+// Add scroll event listener to window
+window.addEventListener('scroll', function() {
+  // Check if the window scroll position is greater than 300 pixels
+  if (window.scrollY > 300) {
+    // If so, add 'show' class to the button
+    btn.classList.add('show');
+  } else {
+    // Otherwise, remove 'show' class from the button
+    btn.classList.remove('show');
+  }
+});
+
+// Add click event listener to the button
+btn.addEventListener('click', function(e) {
+  e.preventDefault(); // Prevent default anchor behavior
+  // Smooth scroll to the top of the page
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
